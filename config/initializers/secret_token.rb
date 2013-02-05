ba484f4b740882bba49afc0d0f923ef4a3ec50bb
Josh Elkin <josh.elkin@gmail.com>
2013-02-05 06:59:41 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hartle2::Application.config.secret_token = '9b757c37bcbce89cb493fd65837ac693d02b2190b8a14b0b964b6b9ebbb3e590ddab7cdfcfc435e93edd0b50374ffcbc9fcf6dd15e60a98ddbfa2dc4b32f744b'
